98301ca6d7e92634c7a23132a70d2b229772ab0b
lagleki <gleki.is.my.name@gmail.com>
2015-03-24 16:13:17 +0300

    Create altatufa-builder.js

---
+++ altatufa-builder.js
@@ -0,0 +1,17 @@
+// // load peg.js and the file system module
+var fs = require("fs")
+var PEG = require("pegjs")
+// // read peg and build a parser
+var camxes_peg = fs.readFileSync("\altatufa.js.peg").toString();
+var camxes = PEG.buildParser(camxes_peg, {cache: true});
+// // write to a file
+// fs.writeFileSync("\camxes.js", camxes.toSource());
+var fd = fs.openSync("\altatufa.js", 'w+');
+var buffer = new Buffer('var camxes = ');
+fs.writeSync(fd, buffer, 0, buffer.length);
+buffer = new Buffer(camxes.toSource());
+fs.writeSync(fd, buffer, 0, buffer.length);
+buffer = new Buffer("\n\nmodule.exports = camxes;\n\nterm = process.argv[2];\nif (term !== undefined && typeof term.valueOf() === 'string')\n  console.log(JSON.stringify(camxes.parse(term)));\n\n");
+fs.writeSync(fd, buffer, 0, buffer.length);
+fs.close(fd);
+

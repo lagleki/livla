506704fae5278550c3fa2d78e8faf46208d5e3c4
lagleki <gleki.is.my.name@gmail.com>
2015-04-30 00:25:10 -0700

    saving "esperanto" dictionary on remote git, fixes to Muplis and English Sutsis

--- i/en/worker.js
+++ i/en/worker.js
@@ -1,5 +1,5 @@
 window = this;
-importScripts('../data/parsed-en.js?version=9', '../lujvo.js?version=9', '../lib/fullproof/unicode/categ_letters_numbers.js', '../lib/fullproof/unicode/normalizer_lowercase_nomark.js', '../lib/fullproof/unicode/unicode.js', '../lib/fullproof/analyzers.js', '../lib/fullproof/normalizers.js', '../lib/fullproof/misc/dataloader.js', '../lib/fullproof/capabilities.js', '../lib/fullproof/utils.js', '../lib/fullproof/common-engine.js', '../lib/fullproof/boolean-engine.js', '../lib/fullproof/resultsets.js', '../lib/fullproof/storemanager.js', '../lib/fullproof/stores/memory_store.js', '../search.js');
+importScripts('../data/parsed-en.js?version=6', '../lujvo.js', '../lib/fullproof/unicode/categ_letters_numbers.js', '../lib/fullproof/unicode/normalizer_lowercase_nomark.js', '../lib/fullproof/unicode/unicode.js', '../lib/fullproof/analyzers.js', '../lib/fullproof/normalizers.js', '../lib/fullproof/misc/dataloader.js', '../lib/fullproof/capabilities.js', '../lib/fullproof/utils.js', '../lib/fullproof/common-engine.js', '../lib/fullproof/boolean-engine.js', '../lib/fullproof/resultsets.js', '../lib/fullproof/storemanager.js', '../lib/fullproof/stores/memory_store.js', '../search.js');
 
 var searchId;
 this.onmessage = function(ev) {

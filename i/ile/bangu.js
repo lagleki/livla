4a81a11f3dc2bd33043fd5d0433345903c79002e
lagleki <gleki.is.my.name@gmail.com>
2015-06-24 01:03:14 -0700

    adding a new Interlingua sutysisku

---
+++ i/ile/bangu.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+window.bangulo = "Loading dictionary ...";
+window.bangubuild = 'Building index...';
+window.bangudesc = "<p><b>la sutysisku</b> is a fast dictionary for Occidental / Interlingue language.<p>- Can I use it if I lose internet access?<br>- Yes, if this happens try opening this very address from this browser.<br> It should still open. A latest version of Chrome, Safari or Firefox is required.<br/>For more resources visit <a href='https://www.facebook.com/groups/tiddlydict' target='_blank'>TiddlyDict Facebook group</a>.</p><div id='searchengine' style='position: absolute;right: 10px;'><i><a href='#' title='en-sutysisku' onclick=\"window.external.AddSearchProvider('http://mw.lojban.org/extensions/ilmentufa/i/ile/sisku.xml')\">Add as a search engine</a></i></div>";
+window.bangusisku='search';
+window.bangudecomp = 'decomposing ...';
+window.bangu = 'interlingue';
+window.cllnotci = 'The Reference Grammar on this topic:';
+window.arrcll = {};

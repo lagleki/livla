17daf294f24e41a0fd81239d1e15771d46757271
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2015-06-24 10:30:54 +0300

    update ile

--- ircbot/livla.js
+++ ircbot/livla.js
@@ -199,7 +199,7 @@ var updatexmldumps = function (callback) {
 			});
 		});
 	}catch(err){console.log('Error when autoupdating: ' + err);}
-	sutsisningau("zamenhofo");sutsisningau("laadan");labangu();
+	sutsisningau("zamenhofo");sutsisningau("laadan");sutsisningau("ile");labangu();
 	//updategloss();# not yet ready function
 };
 var xmlDocEn = libxmljs.parseXml(fs.readFileSync(path.join(__dirname,"dumps","en" + ".xml"),{encoding: 'utf8'}));//store en dump in memory

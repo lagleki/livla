1650e72f269e0b18c00602abee3e8d79e9ebe788
lagleki <gleki.is.my.name@gmail.com>
2015-07-17 12:02:43 -0700

    altatufa minor fixes

--- mahantufa/altatufa.js
+++ mahantufa/altatufa.js
@@ -2050,76 +2050,47 @@ var camxes = (function(){
         pos0 = pos;
         pos1 = pos;
         pos2 = pos;
-        result1 = parse_COI_clause();
-        if (result1 !== null) {
-          result2 = parse_loi_NAICAI_rule();
-          result2 = result2 !== null ? result2 : "";
-          if (result2 !== null) {
-            result1 = [result1, result2];
-          } else {
-            result1 = null;
-            pos = pos2;
-          }
-        } else {
-          result1 = null;
-          pos = pos2;
-        }
-        if (result1 !== null) {
-          result0 = [];
-          while (result1 !== null) {
-            result0.push(result1);
-            pos2 = pos;
-            result1 = parse_COI_clause();
-            if (result1 !== null) {
-              result2 = parse_loi_NAICAI_rule();
-              result2 = result2 !== null ? result2 : "";
-              if (result2 !== null) {
-                result1 = [result1, result2];
-              } else {
-                result1 = null;
-                pos = pos2;
-              }
-            } else {
-              result1 = null;
-              pos = pos2;
-            }
-          }
-        } else {
-          result0 = null;
-        }
+        result0 = parse_COI_clause();
         if (result0 !== null) {
-          result1 = parse_DOI_clause();
+          result1 = parse_loi_NAICAI_rule();
+          result1 = result1 !== null ? result1 : "";
           if (result1 !== null) {
             result0 = [result0, result1];
           } else {
             result0 = null;
-            pos = pos1;
+            pos = pos2;
           }
         } else {
           result0 = null;
-          pos = pos1;
+          pos = pos2;
         }
-        if (result0 === null) {
-          pos1 = pos;
+        if (result0 !== null) {
+          result1 = [];
           pos2 = pos;
-          result0 = parse_COI_clause();
-          if (result0 !== null) {
-            result1 = parse_loi_NAICAI_rule();
-            result1 = result1 !== null ? result1 : "";
-            if (result1 !== null) {
-              result0 = [result0, result1];
+          result2 = parse_COI_clause();
+          if (result2 === null) {
+            result2 = parse_DOI_clause();
+          }
+          if (result2 !== null) {
+            result3 = parse_loi_NAICAI_rule();
+            result3 = result3 !== null ? result3 : "";
+            if (result3 !== null) {
+              result2 = [result2, result3];
             } else {
-              result0 = null;
+              result2 = null;
               pos = pos2;
             }
           } else {
-            result0 = null;
+            result2 = null;
             pos = pos2;
           }
-          if (result0 !== null) {
-            result1 = [];
+          while (result2 !== null) {
+            result1.push(result2);
             pos2 = pos;
             result2 = parse_COI_clause();
+            if (result2 === null) {
+              result2 = parse_DOI_clause();
+            }
             if (result2 !== null) {
               result3 = parse_loi_NAICAI_rule();
               result3 = result3 !== null ? result3 : "";
@@ -2133,24 +2104,23 @@ var camxes = (function(){
               result2 = null;
               pos = pos2;
             }
-            while (result2 !== null) {
-              result1.push(result2);
-              pos2 = pos;
-              result2 = parse_COI_clause();
-              if (result2 !== null) {
-                result3 = parse_loi_NAICAI_rule();
-                result3 = result3 !== null ? result3 : "";
-                if (result3 !== null) {
-                  result2 = [result2, result3];
-                } else {
-                  result2 = null;
-                  pos = pos2;
-                }
-              } else {
-                result2 = null;
-                pos = pos2;
-              }
-            }
+          }
+          if (result1 !== null) {
+            result0 = [result0, result1];
+          } else {
+            result0 = null;
+            pos = pos1;
+          }
+        } else {
+          result0 = null;
+          pos = pos1;
+        }
+        if (result0 === null) {
+          pos1 = pos;
+          result0 = parse_DOI_clause();
+          if (result0 !== null) {
+            result1 = parse_loi_NAICAI_rule();
+            result1 = result1 !== null ? result1 : "";
             if (result1 !== null) {
               result0 = [result0, result1];
             } else {
@@ -2160,9 +2130,6 @@ var camxes = (function(){
           } else {
             result0 = null;
             pos = pos1;
-          }
-          if (result0 === null) {
-            result0 = parse_DOI_clause();
           }
         }
         if (result0 !== null) {

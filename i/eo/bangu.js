31e22f0e12886c308919f14f25175e92827ebdde
lagleki <gleki.is.my.name@gmail.com>
2015-09-16 01:45:56 -0700

    GUI improvements to sutysisku

--- i/eo/bangu.js
+++ i/eo/bangu.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 window.bangulo = "Ŝargando vortaron ...";
 window.bangubuild = 'Konstruando indekson...';
-window.bangudesc = "<p><b>la sutysisku</b> estas rapida vortara ilo por la lingvo Loĵbana.<p>- Ĉu mi povas ĝin uzi tiam, kiam mi estas malkonektite el la Interreto?<br>- Jes, tiaokaze provu malfermi tiun adreson per via komputilo.<br>Ĝi devus malfermiĝi malgraŭe. Necesas Freŝdata versio de Chrome, Safari aŭ Firefox.<br><br>Por pluaj resursoj, vizitu <a href='http://mw.lojban.org' target='_blank'>la Loĵbanan Vikion.</a>.</p><div id='searchengine' style='position: absolute;right: 10px;'><i><a href='#' title='en-sutysisku' onclick=\"window.external.AddSearchProvider('http://mw.lojban.org/extensions/ilmentufa/i/eo/sisku.xml')\">Adici kiel serĉilo</a></i></div>";
+window.bangudesc = "<p><b>la sutysisku</b> estas rapida vortara ilo por la lingvo Loĵbana.<p>- Ĉu mi povas ĝin uzi tiam, kiam mi estas malkonektite el la Interreto?<br>- Jes, tiaokaze provu malfermi tiun adreson per via komputilo.<br>Ĝi devus malfermiĝi malgraŭe. Necesas Freŝdata versio de Chrome, Safari aŭ Firefox.<br><br>Por pluaj resursoj, vizitu <a href='http://mw.lojban.org' target='_blank'>la Loĵbanan Vikion.</a>.</p><p><span id='searchengine'><i><a href='#' title='eo-sutysisku' onclick=\"window.external.AddSearchProvider('https://mw.lojban.org/extensions/ilmentufa/i/eo/sisku.xml')\">Adici kiel serĉilo</a></i></span></p>";
 window.bangusisku='serĉi';
 window.bangudecomp = 'fendando ...';
 siskudescr="Search the Lojban dictionary la jbovlaste.";

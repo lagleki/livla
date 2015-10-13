023428d2bad53342ac4741ac90b02723f1e1e444
lagleki <gleki.is.my.name@gmail.com>
2015-10-13 04:56:34 -0700

    fixing long words in la sutysisku

--- ircbot/livla.js
+++ ircbot/livla.js
@@ -8,7 +8,7 @@ var interm=2900;
 fram="../../../files/fndata-1.5/frame";
 // Default configuration, may be modified by “loadConfig”, with the content of
 // “~/.livla/config.json.
-var tcan='#lojban,#ckule,#tokipona,#jbosnu,#jboguhe';
+var tcan='#lojban,#ckule,#tokipona,#jbosnu,#jboguhe,#fanva';
 var livlytcan='##jboselbau';//where la livla talks to la mensi
 var asker='livla';
 var replier='mensi';

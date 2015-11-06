8ae68625a1b9cc7a55eef2761024c9a1e9184fd5
lagleki <gleki.is.my.name@gmail.com>
2015-11-06 09:10:19 -0800

    GUI imporvements to la muplis, dont include orphan sentences into its db

--- ircbot/template.js
+++ ircbot/template.js
@@ -26,10 +26,14 @@ langs.forEach(function(thisa){
 	b = b.replace("%searchurl%","/extensions/ilmentufa/i/"+thisa+"/sisku.xml");
 	b = b.replace("%searchtitle%",thisa+"-sutysisku");
 	try{m = file.match(/titlelogo *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1].replace(/\\\"/g,"\"");}catch(err){m="<span id='plise' style='display: none;'><a id='st' href=\"../\"><img src=\"../sutysisku.png\" height='16' width='16'><span class='site-title'><font color=\"#fff\">la sutysisku</font></span></a></span>";}b = b.replace(/%titlelogo%/g,m);
-	try{m = file.match(/mupliskari1 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="56,136,233";}b = b.replace(/%mupliskari1%/g,m);
-	try{m = file.match(/mupliskari2 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="34,87,213";}b = b.replace(/%mupliskari2%/g,m);
-	try{m = file.match(/mupliskari3 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="38,99,224";}b = b.replace(/%mupliskari3%/g,m);
-	try{m = file.match(/mupliskari4 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="25,65,165";}b = b.replace(/%mupliskari4%/g,m);
+	try{m=file.match(/mupliskari1 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="56,136,233";}b = b.replace(/%mupliskari1%/g,m);
+	try{m=file.match(/mupliskari2 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="34,87,213";}b = b.replace(/%mupliskari2%/g,m);
+	try{m=file.match(/mupliskari3 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="38,99,224";}b = b.replace(/%mupliskari3%/g,m);
+	try{m=file.match(/mupliskari4 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="25,65,165";}b = b.replace(/%mupliskari4%/g,m);
+	try{m=file.match(/grad_pos1 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="0%";}b = b.replace(/%grad_pos1%/g,m);
+	try{m=file.match(/grad_pos2 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="13%";}b = b.replace(/%grad_pos2%/g,m);
+	try{m=file.match(/grad_pos3 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="88%";}b = b.replace(/%grad_pos3%/g,m);
+	try{m=file.match(/grad_pos4 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="100%";}b = b.replace(/%grad_pos4%/g,m);
 	fs.writeFileSync(path.join(__dirname,"../i",thisa,"index.html"), b);
 });
 

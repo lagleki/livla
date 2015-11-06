90a676adb641ab0a7efe6f5f2de7ab2a1be40782
lagleki <gleki.is.my.name@gmail.com>
2015-11-06 05:29:53 -0800

    GUI customization for la muplis

--- ircbot/template.js
+++ ircbot/template.js
@@ -25,8 +25,11 @@ langs.forEach(function(thisa){
 	b = b.replace("%ogurl%","https://mw.lojban.org/extensions/ilmentufa/i/"+thisa+"/index.html");
 	b = b.replace("%searchurl%","/extensions/ilmentufa/i/"+thisa+"/sisku.xml");
 	b = b.replace("%searchtitle%",thisa+"-sutysisku");
-	try{m = file.match(/titlelogo *= *[\"'](.*?)[\"'];*$/)[1].replace(/\\\"/g,"\"");}catch(err){m="<span id='plise' style='display: none;'><a id='st' href=\"../\"><img src=\"../sutysisku.png\" height='16' width='16'><span class='site-title'><font color=\"#fff\">la sutysisku</font></span></a></span>";}
-	b = b.replace("%titlelogo%",m);
+	try{m = file.match(/titlelogo *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1].replace(/\\\"/g,"\"");}catch(err){m="<span id='plise' style='display: none;'><a id='st' href=\"../\"><img src=\"../sutysisku.png\" height='16' width='16'><span class='site-title'><font color=\"#fff\">la sutysisku</font></span></a></span>";}b = b.replace(/%titlelogo%/g,m);
+	try{m = file.match(/mupliskari1 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="56,136,233";}b = b.replace(/%mupliskari1%/g,m);
+	try{m = file.match(/mupliskari2 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="34,87,213";}b = b.replace(/%mupliskari2%/g,m);
+	try{m = file.match(/mupliskari3 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="38,99,224";}b = b.replace(/%mupliskari3%/g,m);
+	try{m = file.match(/mupliskari4 *= *[\"'](.*?)[\"'];*([\n\r]+|$)/)[1];}catch(err){m="25,65,165";}b = b.replace(/%mupliskari4%/g,m);
 	fs.writeFileSync(path.join(__dirname,"../i",thisa,"index.html"), b);
 });
 

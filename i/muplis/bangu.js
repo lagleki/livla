7256a958742cc11949d333060ac8288874b47b95
lagleki <gleki.is.my.name@gmail.com>
2015-11-06 05:29:53 -0800

    GUI customization for la muplis

--- i/muplis/bangu.js
+++ i/muplis/bangu.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 window.bangulo = "ca'o cpacu lo vlaste";
 window.bangubuild = "ca'o zbasu lo indice";
-window.bangudesc = "<p><b>la .muplis.</b> cu sutra tutci lo nu mitysisku lo mupli pe ju'oi <a href='http://tatoeba.org/jbo' target='_blank'>Tatoeba.org</a></p><p>- .i xu mi kakne lo ka pilno la .muplis. vau va'o lo nu na ka'e kibro pilno<br>- .i ja'a go'i .i va'o lo nu kibro kakne na ku cu .e'u troci lo ka gau do lo brauzero cu cpacu lo se judri be lo me dei moi<br> .i la'a sai lo mutpapri cu tolcanci to'e ri'a nai .i lo cnino klesi be la xromium ja la'oi Safari ja la'oi Firefox cu jai sarcu lo nu pilno la .muplis.<br><br>.i lo nu djica co zenba co cilre tu'a la .muplis. cu .e'u banzu lo nu vitke <a href='https://mw.lojban.org' target='_blank'>lo lojbo uitki</a>.</p><span id='searchengine'><i><a href='#' title='jbo-sutysisku' onclick=\"window.external.AddSearchProvider('https://mw.lojban.org/extensions/ilmentufa/i/zuplis/sisku.xml')\">jmina zo'e ne tai lo sisku matra</a></i></span></p>";
+window.bangudesc = "<p><b>la .muplis.</b> cu sutra tutci lo nu mitysisku lo mupli pe ju'oi <a href='http://tatoeba.org/jbo' target='_blank'>Tatoeba.org</a></p><p>- .i xu mi kakne lo ka pilno la .muplis. vau va'o lo nu na ka'e kibro pilno<br>- .i ja'a go'i .i va'o lo nu kibro kakne na ku cu .e'u troci lo ka gau do lo brauzero cu cpacu lo se judri be lo me dei moi<br> .i la'a sai lo mutpapri cu tolcanci to'e ri'a nai .i lo cnino klesi be la xromium ja la'oi Safari ja la'oi Firefox cu jai sarcu lo nu pilno la .muplis.<br><br>.i lo nu djica co zenba co cilre tu'a la .muplis. cu .e'u banzu lo nu vitke <a href='https://mw.lojban.org' target='_blank'>lo lojbo uitki</a>.</p><span id='searchengine'><i><a href='#' title='jbo-sutysisku' onclick=\"window.external.AddSearchProvider('https://mw.lojban.org/extensions/ilmentufa/i/muplis/sisku.xml')\">jmina zo'e ne tai lo sisku matra</a></i></span>";
 window.bangusisku='sisku';
 window.xuzganalojudri = false;
 window.bangudecomp = 'te katna';
@@ -9,3 +9,8 @@ window.arrcll = {};
 siskudescr="Search the dictionary of Lojban sentences la muplis.";
 opdescr = "A dictionary of sentences for Lojban, English and other languages that can work offline.";
 titlelogo = "<span id='plise'><img src='plise.png' height='16' width='16' title='lo staile poi sampu'><img src='pelxuplise.png' height='16' width='16' title='lo staile poi na mutce cinri'><img src='crinoplise.png' height='16' width='16' title='lo jufra poi ka&#39;e xamgu lo di&#39;i fanva'><img src='blabiplise.png' height='16' width='16' title='lo staile poi ka&#39;e pluja'><img src='cicnaplise.png' height='16' width='16' title='na&#39;e catni se ciska staile'><a id='st' href='../'><span class='site-title'><font color='#fff'>la muplis</font></span></a></span>";
+mupliskari1 = "233,168,56";
+mupliskari2 = "213,147,34";
+mupliskari3 = "224,156,38";
+mupliskari4 = "165,114,25";
+//38

3a38bac2c6e1da36ae5a9ea85fd4872e7f3e60b6
lagleki <gleki.is.my.name@gmail.com>
2015-11-10 03:45:52 -0800

    exp. fixes in sutysisku search, copying ext.images to the repo

--- i/laadan/bangu.js
+++ i/laadan/bangu.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 window.bangulo = "Loading dictionary ...";
 window.bangubuild = 'Building index...';
-window.bangudesc = "<p><b>la sutysisku</b> is a fast dictionary for the Laadan language.<p>- Can I use it if I lose internet access?<br>- Yes, if this happens try opening this very address from this browser.<br> It should still open. A latest version of Chrome, Safari or Firefox is required.<br><br>For more resources visit <a href='https://mw.lojban.org' target='_blank'>Lojban Wiki</a>.</p><p><span id='searchengine'><i><a href='#' title='en-sutysisku' onclick=\"window.external.AddSearchProvider('https://mw.lojban.org/extensions/ilmentufa/i/en/sisku.xml')\">Add as a search engine</a></i></span></p>";
+window.bangudesc = "<p><b>la sutysisku</b> is a fast dictionary for the Láadan language.<p>- Can I use it if I lose internet access?<br>- Yes, if this happens try opening this very address from this browser.<br> It should still open. A latest version of Chrome, Safari or Firefox is required.<br><br>For more resources visit <a href='https://mw.lojban.org' target='_blank'>Lojban Wiki</a>.</p><p><span id='searchengine'><i><a href='#' title='laadan-sutysisku' onclick=\"window.external.AddSearchProvider('https://mw.lojban.org/extensions/ilmentufa/i/laadan/sisku.xml')\">Add as a search engine</a></i></span></p>";
 window.bangusisku='search';
 window.xuzganalojudri = false;
 window.bangudecomp = 'decomposing ...';
 window.arrcll = {};
 siskudescr="Search the Laadan dictionary la sutysisku.";
-opdescr = "A dictionary for Laadan, Lojban, English and other languages that can work offline.";
+opdescr = "A dictionary for Láadan, Lojban, English and other languages that can work offline.";
 upperdir = "../muplis.html";
 

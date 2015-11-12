7bd506b474317cf971aeeeb7b929d2b9e4670387
lagleki <gleki.is.my.name@gmail.com>
2015-11-11 23:32:24 -0800

    show welcome tile when manually clearing search field in la sutysisku:

--- i/muplis-eng-pol/bangu.js
+++ i/muplis-eng-pol/bangu.js
@@ -8,7 +8,7 @@ window.bangudecomp = 'decomposing...';
 window.arrcll = {};
 siskudescr="Search la muplis, the dictionary of English and Polish sentences.";
 opdescr = "A dictionary of sentences for English, Polish and other languages that can work offline.";
-titlelogo = "<span id='plise'><img src='plise.png' height='16' width='16' title='lo staile poi sampu'><img src='pelxuplise.png' height='16' width='16' title='lo staile poi na mutce cinri'><img src='crinoplise.png' height='16' width='16' title='lo jufra poi ka&#39;e xamgu lo di&#39;i fanva'><img src='blabiplise.png' height='16' width='16' title='lo staile poi ka&#39;e pluja'><img src='cicnaplise.png' height='16' width='16' title='na&#39;e catni se ciska staile'><a id='st' href='%upper%'><span class='site-title'><font color='#fff'>la muplis</font></span></a></span>";
+titlelogo = "<span id='plise' style='display: none;'><img src='plise.png' height='16' width='16' title='lo staile poi sampu'><img src='pelxuplise.png' height='16' width='16' title='lo staile poi na mutce cinri'><img src='crinoplise.png' height='16' width='16' title='lo jufra poi ka&#39;e xamgu lo di&#39;i fanva'><img src='blabiplise.png' height='16' width='16' title='lo staile poi ka&#39;e pluja'><img src='cicnaplise.png' height='16' width='16' title='na&#39;e catni se ciska staile'><a id='st' href='%upper%'><span class='site-title'><font color='#fff'>la muplis</font></span></a></span>";
 mupliskari1 = "109,179,242";
 mupliskari2 = "84,163,238";
 mupliskari3 = "54,144,240";

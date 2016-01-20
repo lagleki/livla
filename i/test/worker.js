89a275026669b0cd5013cb03b5e4029dcfa197c8
lagleki <gleki.is.my.name@gmail.com>
2016-01-20 06:38:53 -0800

    la utysisku now decomposes by chunks of text too

--- i/test/worker.js
+++ i/test/worker.js
@@ -1,14 +1,6 @@
 window = this;
-importScripts('bangu.js','../data/parsed-en.js', '../lujvo_beta.js', '../sisku_2.js');
+importScripts('bangu.js','../data/parsed-jb.js', '../lujvo_beta.js', '../sisku_2.js');
 var searchId;
-this.onmessage = function(ev) {
-if (ev.data.kind == 'newSearch') {
-		searchId = ev.data.searchId;
-			search(ev.data.query, function(results) {
-				postMessage({kind: 'searchResults', results: results,query:ev.data.query});
-			});
-}
-};
-	
+this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;search(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};
 postMessage({kind: 'loading'});
 postMessage({kind: 'ready'});

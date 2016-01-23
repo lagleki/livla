167a1b0ceb812979dfad2d685a138c5c35722479
lagleki <gleki.is.my.name@gmail.com>
2016-01-23 07:05:17 -0800

    faster loading of la sutysisku

--- i/eo/worker.js
+++ i/eo/worker.js
@@ -1,6 +1,6 @@
 window = this;
 importScripts('bangu.js','../data/parsed-eo.js', '../lujvo_beta.js', '../sisku.js');
-var searchId;
-this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;search(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};
 postMessage({kind: 'loading'});
 postMessage({kind: 'ready'});
+var searchId;
+this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;search(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};

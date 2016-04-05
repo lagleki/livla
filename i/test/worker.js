e9eaa5503f48d1cf5c46e04d81403373f35b385e
lagleki <gleki.is.my.name@gmail.com>
2016-04-05 04:30:06 -0700

    receive feedback

--- i/test/worker.js
+++ i/test/worker.js
@@ -1,6 +1,6 @@
 window = this;
-importScripts('bangu.js','../data/parsed-en.js', '../lujvo_2.js', '../sisku_2.js');
+importScripts('bangu.js','../data/parsed-en.js', '../sisku_2.js');
 postMessage({kind: 'loading'});
 postMessage({kind: 'ready'});
 var searchId;
-this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;search(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};
+this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;sisku(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};

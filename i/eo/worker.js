e2c33d699d8467181a6890d3d7ee303a1ffadb95
lagleki <gleki.is.my.name@gmail.com>
2016-04-06 01:11:05 -0700

    merge la rimni sutysisku with others

--- i/eo/worker.js
+++ i/eo/worker.js
@@ -1,6 +1,6 @@
 window = this;
-importScripts('bangu.js','../data/parsed-eo.js', '../lujvo_beta.js', '../sisku.js');
+importScripts('bangu.js','../data/parsed-eo.js', '../sisku.js');
 postMessage({kind: 'loading'});
 postMessage({kind: 'ready'});
 var searchId;
-this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;search(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};
+this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {searchId = ev.data.searchId;sisku(ev.data.query, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query});});}};

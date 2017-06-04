5cb1049eca4566293098468e2bc1b4e487e66e9e
lagleki <gleki.is.my.name@gmail.com>
2017-06-04 10:44:54 -0700

    gitignore + dir rename

---
+++ sutysisku/ldp/bangu.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+window.nasezvafahi = 'not found';
+window.bangulo = 'Loading dictionary ...';
+window.bangubuild = 'Building index...';
+window.bangudesc = "<p><b>la sutysisku</b> is a fast dictionary for Lingwa de planeta language.<p>- Can I use it if I lose internet access?<br>- Yes, if this happens try opening this very address from this browser.<br> It should still open. A latest version of Chrome, Safari or Firefox is required.<br><br>For more resources visit <a href='https://www.facebook.com/groups/tiddlydict' target='_blank'>Facebook TiddlyDict group</a> or e-mail <a href='mailto:gleki.is.my.name@gmail.com' target='_blank'>gleki.is.my.name@gmail.com</a>.<p><span id='searchengine'><i><a href='#' title='ldp-sutysisku' onclick=\"window.external.AddSearchProvider('https://la-lojban.github.io/sutysisku/ldp/sisku.xml')\">Add as a search engine</a></i></span></p>";
+window.bangusisku='search';
+window.xuzganalojudri = false;
+window.bangudecomp = 'decomposing ...';
+window.arrcll = {};
+siskudescr="Search the Lingwa de Planeta (Lidepla) dictionary la sutysisku.";
+opdescr = "A dictionary for Lidepla (Lingwa de Planeta), Lojban, English and other languages. Can work offline!";
+upperdir = "../muplis.html";
+window.purc = "Last searched: ";
+window.lojbo = false;

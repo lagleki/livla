5e02f749a256a72dc4569bc4a0e93506ca9abe75
lagleki <gleki.is.my.name@gmail.com>
2019-07-20 08:57:06 +0000

    fix zlm-converter

--- skripto/convert-piper-to-zbalermorna.js
+++ skripto/convert-piper-to-zbalermorna.js
@@ -1,13 +1,9 @@
-// a script to convert the comic about la .Piper. into la zbalermorna
-// use the lakmir's font
-var fs = require("fs");
+// a script to convert the comic about la .Piper. in to la zbalermorna
+var fs = require("fs-extra");
 var path = require("path-extra");
-
-var ncp = require("ncp").ncp;
 
 const C = "ptkflscmxbdgvrzjnɩw";
 const V = "aeiouyąęǫḁ";
-ncp.limit = 16;
 
 function formatUnicode(point) {
   return "&#x" + point.toString(16) + ";";
@@ -17,8 +13,10 @@ const krulermorna = function(t) {
   return t
     .replace(/h/g, "'")
     .toLowerCase()
-    .replace(/([aeiouy\.])u([aeiou])/g, "$1w$2")
-    .replace(/([aeiouy\.])i([aeiouy])/g, "$1ɩ$2")
+    .replace(/([aeiouy\. ])u([aeiouy])/g, "$1w$2")
+    .replace(/([aeiouy\. ])i([aeiouy])/g, "$1ɩ$2")
+    .replace(/^u([aeiouy])/g, "w$1")
+    .replace(/^i([aeiouy])/g, "ɩ$1")
     .replace(/au/g, "ḁ")
     .replace(/ai/g, "ą")
     .replace(/ei/g, "ę")
@@ -40,9 +38,14 @@ function translate(text) {
   text = text.replace(/[ \.]+/g, ".");
   text = text.replace(/^([ḁąęǫ])/g, ".$1");
   let out = "";
-  console.log(text);
+  if (debug) console.log(text);
   while (text.length > 0) {
+    let i = 0;
     let changed = false;
+    text = text.replace(
+      /^([wɩ\.])([aeiouy])([ \.]+)(?=[ptkflscmxbdgvrzjnɩw])/g,
+      "$1$2"
+    );
     text = text.replace(/^\.[aeiouyąęǫḁ]'[aeiouyąęǫḁ]/, function(match) {
       const fir = match.charAt(1);
       const sec = match.charAt(3);
@@ -51,22 +54,34 @@ function translate(text) {
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
     text = text.replace(/^'[aeiouyąęǫḁ]/, function(match) {
       const fir = match.charAt(1);
-      start = 0xe2311 + V.indexOf(fir);
+      start = 0xe2310 + V.indexOf(fir) + 1;
       out += formatUnicode(start);
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
     text = text.replace(/^\.[aeiouyąęǫḁ]/, function(match) {
       const fir = match.charAt(1);
-      start = 0xe2450 + V.indexOf(fir) * 16;
+      start = 0xe2300 + V.indexOf(fir) + 1;
       out += formatUnicode(start);
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
     text = text.replace(/^[ptkflscmxbdgvrzjnɩw][aeiouyąęǫḁ]/, function(match) {
       const fir = match.charAt(0);
@@ -76,6 +91,10 @@ function translate(text) {
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
     text = text.replace(/^[ptkflscmxbdgvrzjnɩw]/, function(match) {
       const fir = match;
@@ -84,56 +103,100 @@ function translate(text) {
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
     text = text.replace(/^[aeiouyąęǫḁ]/, function(match) {
       const sec = match;
-      start = 0xe24f1 + V.indexOf(sec);
+      start = 0xe24f0 + V.indexOf(sec) + 1;
       out += formatUnicode(start);
       changed = true;
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
     if (changed) continue;
+
     text = text.replace(/^\./, function(match) {
       const sec = match;
       start = 0xe2300;
       out += formatUnicode(start);
       return "";
     });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
+
+    text = text.replace(/^ /, function(match) {
+      const sec = match;
+      start = 0xe2300;
+      out += formatUnicode(start);
+      return "";
+    });
+    if (debug) {
+      i++;
+      console.log(i, text, out);
+    }
   }
   return out;
 }
 
-//console.log(translate(".i mi noi"));
+let debug = 0;
+//console.log(translate(".i .oi .i pu"));
 //process.exit();
 
-fs.readdir("./", (err, files) => {
-  console.log(files);
-  files.forEach(fl => {
-    let source = path.join(fl, "jb");
-    let dest = path.join(fl, "zj");
-    ncp(source, dest, function(err) {
-      fs.readdir(dest, (err, files) => {
-        (files || []).forEach(file => {
-          let tetcidu = path.join(__dirname, dest, file);
-          if (file.slice(-4) === ".svg") {
-            let data = fs.readFileSync(tetcidu, "utf8");
-            let str = data.split(">");
-            for (let i = 0; i < str.length; i++) {
-              let gun = str[i].split("<");
-              let f = gun[0];
-              if (/^[a-z \.0-9\']+$/.test(f)) {
-                gun[0] = translate(f);
-              }
-              str[i] = gun.join("<");
-            }
-            str = str.join(">");
-
-            fs.writeFileSync(tetcidu+".svg", str, "utf8", function(err) {
-              if (err) return console.log(err);
-            });
-          }
-        });
-      });
+async function loop() {
+  const files = await new Promise(resolve => {
+    fs.readdir("../webcomics", (err, files) => {
+      resolve(files);
     });
   });
-});
+  for (let fl of files) {
+    let src = path.join("../webcomics", fl, "lang/jb");
+    let dest = path.join("../webcomics", fl, "lang/jz");
+    try {
+      fs.copySync(src, dest, { overwrite: true });
+      const files = await new Promise(resolve => {
+        fs.readdir(dest, (err, files) => {
+          resolve(files);
+        });
+      });
+      for (let file of files) {
+        let tetcidu = path.join(__dirname, dest, file);
+        if (file.slice(-4) === ".svg" && file.slice(-8) !== ".svg.svg") {
+          let data = fs.readFileSync(tetcidu, "utf8");
+          let str = data.split(">");
+          for (let i = 0; i < str.length; i++) {
+            let gun = str[i].split("<");
+            let f = gun[0];
+            if (/^[a-z \.0-9\']+$/.test(f)) {
+              gun[0] = translate(f);
+            }
+            str[i] = gun.join("<");
+          }
+          str = str.join(">");
+          str = str.replace(/\bLavi\b/g, "zbalermorna");
+          str = str.replace(/font-size: *(.+?) *px/gm, function(ma, p1) {
+            const size = parseFloat(p1) * 1.1;
+            return `font-size:${size}px`;
+          });
+          try {
+            fs.unlinkSync(tetcidu + ".svg");
+          } catch (err) {}
+          fs.writeFileSync(tetcidu + ".svg", str, "utf8", function(err) {
+            if (err) return console.log(err);
+          });
+        }
+      }
+    } catch (e) {}
+  }
+}
+
+(async () => {
+  loop();
+})();

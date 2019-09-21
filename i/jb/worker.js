88c5c8aa14ef2d55527848bb92c10d1711537358
lagleki <gleki.is.my.name@gmail.com>
2019-09-21 18:28:17 +0000

    add new v. of sutysisku

--- i/jb/worker.js
+++ i/jb/worker.js
@@ -1,5 +1,5 @@
 window = this;var sorcu={};var bau = location.href.split('/').slice(-2)[0];if (bau==='cipra'){bau='en';}
 postMessage({kind: 'loading'});
-importScripts('bangu.js','../data/parsed-jb.js?sisku=2019_09_20_18_12_27', '../sisku.js?sisku=2019_09_20_18_12_27');
+importScripts('bangu.js','../data/parsed-jb.js?sisku=2019_09_21_17_37_31', '../sisku.js?sisku=2019_09_21_17_37_31');
 postMessage({kind: 'ready'});
 this.onmessage = function(ev) {if (ev.data.kind == 'newSearch') {sisku(ev.data, function(results) {postMessage({kind: 'searchResults', results: results,query:ev.data.query})})}}

522919457a3391c465b29e0afdbe70ce810f4ab8
lagleki <gleki.is.my.name@gmail.com>
2020-03-12 06:34:51 -0700

    add sutysisku directory

---
+++ sutysisku/src/en/bangu.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+window.nasezvafahi = "No definition has been found";
+window.rimni = "la rimni sutysisku";
+window.bangudecomp = "decomposing ...";
+window.bangubuild = "Building index...";
+window.xuzganalojudri = true;
+window.judri = "http://jbovlaste.lojban.org/dict/";
+window.cllnotci = "The Reference Grammar on this topic:";
+window.purc = "Last searched: ";
+window.bangu = ".inglic.";
+window.cll_url = "https://lojban.pw/cll/uncll-1.2.5/xhtml_section_chunks/";

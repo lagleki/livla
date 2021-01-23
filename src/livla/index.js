b205d0b66e65263166c4db8424a153d750a421d0
lagleki <gleki.is.my.name@gmail.com>
2021-01-23 03:05:03 -0800

    fix bugs in the new sutysisku

--- src/livla/index.js
+++ src/livla/index.js
@@ -4,7 +4,6 @@ const lg = console.log.bind(console)
 // livla bot
 const fs = require('fs')
 const path = require('path-extra')
-const ospath = require('ospath')
 const R = require('ramda')
 const lojban = require('lojban')
 const he = require('he')
@@ -81,7 +80,8 @@ let tcan =
 let localConfig
 let nuzbytcan = '#lojban'
 let replier = 'mensi'
-let httpPort = 3020
+let httpPort = 3000
+let http_sutysisku_port = 3001
 let password = ''
 let server = 'irc.freenode.net'
 let twitter_id = 'good_opinions,opinions_good,cunsku'
@@ -172,7 +172,6 @@ const loadConfig = () => {
   consumer_secret = either(localConfig, 'consumer_secret', '')
   access_token_key = either(localConfig, 'access_token_key', '')
   access_token_secret = either(localConfig, 'access_token_secret', '')
-  httpPort = either(localConfig, 'httpPort', httpPort)
   twitter_id = either(localConfig, 'twitter_id', twitter_id)
   arr_twitter_id = twitter_id.split(',')
 
@@ -843,8 +842,7 @@ const ningau_paladeksi_sutysisku = async ({ json, tegerna }) => {
   }
 
   let t = path.join(
-    __dirname,
-    '../build/sutysisku/data',
+    '/livla/build/sutysisku/data',
     `parsed-${tegerna}.blob.json`
   )
   fs.writeFileSync(t, JSON.stringify(outp))
@@ -862,15 +860,13 @@ const ningau_palasutysisku = async (language, lojbo) => {
         lojbo,
       })
     let t = path.join(
-      __dirname,
-      '../build/sutysisku/data',
+      '/livla/build/sutysisku/data',
       `parsed-${language}.${format}`
     )
     fs.writeFileSync(`${t}.temp`, pars)
     fs.renameSync(`${t}.temp`, t)
     t = path.join(
-      __dirname,
-      `../build/sutysisku/${language}/`,
+      `/livla/build/sutysisku/${language}/`,
       'webapp.appcache'
     )
     const d = new Date()
@@ -895,7 +891,8 @@ const ningau_palasutysisku = async (language, lojbo) => {
 const tcepru = (lin, sendTo, socket) => {
   const exec = require('child_process').exec
   exec(
-    `${path.join(__dirname, '../tcepru/./parser')} <<<"${lin}" 2>/dev/null`,
+    `${home_path}/src/tcepru/parser <<<"${lin}" 2>/dev/null`,
+    {shell: '/bin/bash'},
     (error, stdout, stderr) => {
       lin = stdout
       if (error !== null) {
@@ -913,10 +910,8 @@ const tcepru = (lin, sendTo, socket) => {
 const jbofihe = (lin, sendTo, socket) => {
   const exec = require('child_process').exec
   exec(
-    `${path.join(
-      __dirname,
-      '../jbofihe/./jbofihe'
-    )} -ie -cr <<<"${lin}" 2>/dev/null`,
+    `${home_path}/src/jbofihe/jbofihe -ie -cr <<<"${lin}" 2>/dev/null`,
+    {shell: '/bin/bash'},
     (error, stdout, stderr) => {
       console.log(error, stdout)
       lin = stdout
@@ -1049,7 +1044,7 @@ async function downloadSingleDump({ language, erroredLangs }) {
 }
 
 async function updateXmlDumps() {
-  await mkdirp(path.join(__dirname, '../build/sutysisku/data'))
+  await mkdirp(path.join('/livla/build/sutysisku/data'))
   let erroredLangs = []
   for (const language of langs) {
     erroredLangs = await downloadSingleDump({ language, erroredLangs })
@@ -1547,21 +1542,8 @@ if (!config.disableIrcBots) {
   })
 }
 
-// NAXLE
-
-// NEVER use a Sync function except at start-up!
-const index = fs.readFileSync(`${__dirname}/naxle.html`)
-
-// Send index.html to all requests
-const app = require('http').createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-  })
-  res.end(index)
-})
-
 // Socket.io server listens to our app
-const io = require('socket.io').listen(app)
+const io = require('socket.io')(httpPort)
 
 const IPFromRequest = ({ headers, connection, params }) => {
   let ip
@@ -1609,4 +1591,10 @@ io.sockets.on('connection', (socket) => {
   })
 })
 
-app.listen(httpPort)
+const express = require('express');
+const app = express();
+const public = '/livla/build'
+
+app.use('/', express.static(public));
+
+app.listen(http_sutysisku_port);

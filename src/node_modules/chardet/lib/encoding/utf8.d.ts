8558b6a7b97f6bc42e6961ebe141d915acb39852
lagleki <gleki.is.my.name@gmail.com>
2021-07-12 08:40:47 +0000

    fix

--- src/node_modules/chardet/lib/encoding/utf8.d.ts
+++
@@ -1,5 +0,0 @@
-import { Context, Recogniser } from '.';
-export default class Utf8 implements Recogniser {
-    name(): string;
-    match(det: Context): any;
-}
--- src/node_modules/chardet/src/encoding/utf8.test.ts
+++
@@ -1,9 +0,0 @@
-import * as chardet from '..';
-
-describe('UTF-8', () => {
-  it('should return UTF-8', () => {
-    expect(
-      chardet.detectFileSync(__dirname + '/../test/data/encodings/utf8')
-    ).toBe('UTF-8');
-  });
-});

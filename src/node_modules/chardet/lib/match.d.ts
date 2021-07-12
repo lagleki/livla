e57643040c9393eb2db3129812c63002031e281d
lagleki <gleki.is.my.name@gmail.com>
2021-07-12 08:40:47 +0000

    fix

--- src/node_modules/chardet/lib/match.d.ts
+++
@@ -1,7 +0,0 @@
-export interface Match {
-    confidence: number;
-    name: string;
-    lang: string;
-}
-declare const _default: (det: any, rec: any, confidence: any, name: any, lang: any) => Match;
-export default _default;

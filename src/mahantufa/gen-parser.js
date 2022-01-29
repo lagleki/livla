288711c7a4f9ee622d138cd0c6f8e434a2d686cf
lagleki <gleki.is.my.name@gmail.com>
2022-01-29 10:46:12 +0300

    Update gen-parser.js

--- src/mahantufa/gen-parser.js
+++ src/mahantufa/gen-parser.js
@@ -6,22 +6,21 @@ const fileName = process.argv[2]
 
 const filePathCore = path.join(__dirname, `../mahantufa/${fileName}`)
 const grammarSrc = fs.readFileSync(`${filePathCore}.peg`).toString().split("\n")
-    .map(line => line.trim().replace(/^#.*?$/, '').trim().replace(/^([a-z0-9]+) *<- */i, '$1 = ')).filter(Boolean).join("\n")
+    .map(line => line.trim().replace(/^#.*?$/, '').trim().replace(/^([a-zA-Z0-9]+)[\t ]*<-[\t ]*/, '$1 = ')).filter(Boolean).join("\n")
     
 const ruleNames = (grammarSrc) => {
     return grammarSrc.split("\n").map(_ => _.split("=")[0].trim()).filter(Boolean)
 };
 
-// read peg and build a parser
 const generated_parser = require('peggy').generate(grammarSrc, {
-    cache: true,
-    trace: false,
+    cache: false,
+    trace: true,
     output: 'source',
     allowedStartRules: ruleNames(grammarSrc),
     format: 'commonjs',
     plugins: [new SyntacticActionsPlugin()]
 })
-// write to a file
+
 fs.writeFileSync(filePathCore + ".unwrapped.js", generated_parser, { encoding: 'utf8' })
 
 const { minify } = require("terser");

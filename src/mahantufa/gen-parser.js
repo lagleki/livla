a3e04ef38edf444c6fc63107aba44a1b3ce27213
lagleki <gleki.is.my.name@gmail.com>
2022-02-18 18:37:27 +0000

    fix #580; fix #581

--- src/mahantufa/gen-parser.js
+++ src/mahantufa/gen-parser.js
@@ -6,15 +6,17 @@ const fileName = process.argv[2]
 
 const filePathCore = path.join(__dirname, `../mahantufa/${fileName}`)
 const grammarSrc = fs.readFileSync(`${filePathCore}.peg`).toString().split("\n")
-    .map(line => line.trim().replace(/^#.*?$/, '').trim().replace(/^([a-zA-Z0-9]+)[\t ]*<-[\t ]*/, '$1 = ')).filter(Boolean).join("\n")
-    
+    .map(line => line.trim().replace(/^[^a-zA-Z0-9_].*?$/, '').trim().replace(/^([a-zA-Z0-9_]+)[\t ]*<-[\t ]*/, '$1 = '))
+    .filter(Boolean)
+    .join("\n")
+
 const ruleNames = (grammarSrc) => {
     return grammarSrc.split("\n").map(_ => _.split("=")[0].trim()).filter(Boolean)
 };
 
 const generated_parser = require('peggy').generate(grammarSrc, {
-    cache: false,
-    trace: true,
+    cache: true,
+    trace: false,
     output: 'source',
     allowedStartRules: ruleNames(grammarSrc),
     format: 'commonjs',

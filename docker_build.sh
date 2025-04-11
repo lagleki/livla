cce10040e88ff6228710cf437c72483dbf9e05b0
lagleki <gleki.is.my.name@gmail.com>
2025-04-11 06:27:12 -0700

    update links

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t livla .
+docker build --no-cache -t livla .
--- docker_start.sh
+++ docker_start.sh
@@ -9,7 +9,6 @@ docker run \
   --log-opt max-size=20k --log-opt max-file=1 \
   -v $(pwd)/src:/livla/src/:Z \
   -v $(pwd)/config:/livla/config/:Z \
-  -v $(pwd)/node_modules:/livla/node_modules/:Z \
   -v $(pwd)/build:/livla/build/:Z \
   -p 3020:3000 \
   -p 3021:3001 \
